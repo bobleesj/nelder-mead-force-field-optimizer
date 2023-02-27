cab825bdf9708e59b3f936844ca12376307ef003
“bobleesj” <bobleesj@gmail.com>
2023-02-27 15:23:14 -0500

    applied neader mead for 2 Ar atoms

--- xyz.py
+++ xyz.py
@@ -2,6 +2,7 @@ import xyz_py as xyzp
 import numpy as np
 from scipy.optimize import minimize 
 
+
 # Global variables
 dist = 0
 
@@ -35,14 +36,61 @@ Double-checked using https://www.calculatorsoup.com
 d = 2.962908
 '''
 
+print("Manually determining r")
+x1 = 0.26765
+y1 = 2.64288
+z1 = 0
+x2 = 0.02059
+y2 = -0.30971
+z2 = 0
+r = np.sqrt((x1 - x2)**2 + (y1 - y2)**2 + (z1 - z2)**2)
+print(r)
+
 # Determine LJ for 2 Ar atoms
 potential = LJPotential(dist)
 potential_rounded = round(potential, 2)
 print(potential_rounded, "(kJ/mol) is the LJ potential at d =", dist_rounded, "Å \n")
 
-# How to 
+# Run more than 1 variable
+def func(x):
+  
+  y = 4 * eps * ((alpha/x)**12 - (alpha/x)**6)
+  return y
+
+min = minimize(func, 2.5, method="nelder-mead")
+# print(min)
 
 
+def f(params):
+    # print(params)  # <-- you'll see that params is a NumPy array
+    x1, x2, y1, y2, z1, z2 = params # <-- for readability you may wish to assign names to the component variables
+    # r = np.sqrt((x1 - x2)**2 + (y1 - y2)**2 + (z1 - z2)**2)
+    # r = np.sqrt((x1 - x2)**2 + (y1 - y2)**2 + (z1 - z2)**2)
+    return 4 * eps * ((alpha/np.sqrt((x1 - x2)**2 + (y1 - y2)**2 + (z1 - z2)**2))**12 - (alpha/np.sqrt((x1 - x2)**2 + (y1 - y2)**2 + (z1 - z2)**2))**6)
 
+initial_guess = [0, 10, 0, 10, 0, 10]
+result = minimize(f, initial_guess, method="nelder-mead")
+if result.success:
+    fitted_params = result.x
+    x1 = fitted_params[0]
+    x2 = fitted_params[1]
+    y1 = fitted_params[2]
+    y2 = fitted_params[3]
+    z1 = fitted_params[4]
+    z2 = fitted_params[5]
+    print("x1:", x1)
+    print("x2:", x2)
+    print("y1:", y1)
+    print("y2:", y2)
+    print("z1:", z1)
+    print("z2:", z2)
+    
+
+    r = np.sqrt((x1 - x2)**2 + (y1 - y2)**2 + (z1 - z2)**2)
+    print(r)
+
+
+else:
+    raise ValueError(result.message)
 
 
